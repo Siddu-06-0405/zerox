2d9d877c537971079d7d57a6820ae28f6a3cef52
siddhuseelam <siddhuseelam@gmail.com>
2025-03-04 03:20:13 +0530

    made frontend changes and also changed few ports from 5000 to 5001

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import useLogin from "../hooks/useLogin";
+import { Button } from "@/components/ui/button";
+import { LoginForm } from "@/components/login-form";
+
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
 
 const Login = () => {
 	const [username, setUsername] = useState("");
@@ -14,93 +28,73 @@ const Login = () => {
 	};
 
 	return (
-		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-					Login
-					<span className='text-blue-500'> Company Name</span>
-				</h1>
-
-				<form onSubmit={handleSubmit}>
-					<div>
-						<label className='label p-2'>
-							<span className='text-base label-text'>Username</span>
-						</label>
-						<input
-							type='text'
-							placeholder='Enter username'
-							className='w-full input input-bordered h-10'
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-						/>
-					</div>
-
-					<div>
-						<label className='label'>
-							<span className='text-base label-text'>Password</span>
-						</label>
-						<input
-							type='password'
+		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+			<div className="w-full max-w-sm">
+					
+				<div >
+					  <Card>
+						<CardHeader>
+						  <CardTitle className="text-2xl">Login to Zerox</CardTitle>
+						  <CardDescription>
+							Enter your username below to login to your account
+						  </CardDescription>
+						</CardHeader>
+						<CardContent>
+						<form onSubmit={handleSubmit}>
+							<div className="flex flex-col gap-6">
+							  <div className="grid gap-3">
+								<Label htmlFor="username">Username</Label>
+								<Input
+								  type='text'
+								  placeholder='Enter username'
+								  className='w-full input input-bordered h-10'
+								  value={username}
+								  id = 'username'
+								  onChange={(e) => setUsername(e.target.value)}
+								/>
+							  </div>
+							  <div className="grid gap-3">
+								<div className="flex items-center">
+								  <Label htmlFor="password">Password</Label>
+								  <a
+									href="#"
+									className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+								  >
+									Forgot your password?
+								  </a>
+								</div>
+								<Input type='password'
 							placeholder='Enter Password'
 							className='w-full input input-bordered h-10'
 							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
+							onChange={(e) => setPassword(e.target.value)} />
+							  </div>
+							  <div className="flex flex-col gap-3">
+								<Button type="submit" className="w-full">
+								  Login
+								</Button>
+								<Button variant="outline" className="w-full">
+								  Login with Google
+								</Button>
+							  </div>
+							</div>
+							<div className="mt-4 text-center text-sm">
+							  Don&apos;t have an account?{" "}
+							  <a href="/signup" className="underline underline-offset-4">
+								Sign up
+							  </a>
+							</div>
+						  </form>
+						</CardContent>
+					  </Card>
 					</div>
-					<Link to='/signup' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
-						{"Don't"} have an account?
-					</Link>
+				
+					
 
-					<div>
-						<button className='btn btn-block btn-sm mt-2' disabled={loading}>
-							{loading ? <span className='loading loading-spinner '></span> : "Login"}
-						</button>
-					</div>
-				</form>
+
 			</div>
 		</div>
 	);
 };
 export default Login;
 
-// STARTER CODE FOR THIS FILE
-// const Login = () => {
-// 	return (
-// 		<div className='flex flex-col items-center justify-center min-w-96 mx-auto'>
-// 			<div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-// 				<h1 className='text-3xl font-semibold text-center text-gray-300'>
-// 					Login
-// 					<span className='text-blue-500'> ChatApp</span>
-// 				</h1>
-
-// 				<form>
-// 					<div>
-// 						<label className='label p-2'>
-// 							<span className='text-base label-text'>Username</span>
-// 						</label>
-// 						<input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' />
-// 					</div>
-
-// 					<div>
-// 						<label className='label'>
-// 							<span className='text-base label-text'>Password</span>
-// 						</label>
-// 						<input
-// 							type='password'
-// 							placeholder='Enter Password'
-// 							className='w-full input input-bordered h-10'
-// 						/>
-// 					</div>
-// 					<a href='#' className='text-sm  hover:underline hover:text-blue-600 mt-2 inline-block'>
-// 						{"Don't"} have an account?
-// 					</a>
-
-// 					<div>
-// 						<button className='btn btn-block btn-sm mt-2'>Login</button>
-// 					</div>
-// 				</form>
-// 			</div>
-// 		</div>
-// 	);
-// };
-// export default Login;
